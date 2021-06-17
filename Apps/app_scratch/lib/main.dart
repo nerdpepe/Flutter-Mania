bfead8b30ac4da02cc525c3ad5a3df6446d599f0
Aditya Bhardwaj <aditbhardwj@gmail.com>
2021-06-17 11:10:04 +0530

    Added Elevated Buttons

--- Apps/app_scratch/lib/main.dart
+++ Apps/app_scratch/lib/main.dart
@@ -5,6 +5,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // for onPressed button
+  void answerChosen() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     // home : Text('hello') -> sets only text
@@ -15,8 +20,23 @@ class MyApp extends StatelessWidget {
           title: Text('in AppBar'),
           backgroundColor: Colors.purple[200],
         ),
-        body: Text('Text in Body'),
-        backgroundColor: Colors.blueAccent,
+        body: Column(
+          children: [
+            Text('Question'),
+            ElevatedButton(
+              onPressed: answerChosen,
+              child: Text('Option 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 2 chosen!'),
+              child: Text('Option 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 3 chosen!'),
+              child: Text('Option 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
