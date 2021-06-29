c690982bfc3a935e77f2d68539188e8a40e9bef2
Aditya Bhardwaj <aditbhardwj@gmail.com>
2021-06-29 18:30:08 +0530

    Expense Tracker initialized

---
+++ Apps/expense_tracker/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import './transaction.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Card(
+            child: Text('Upper Card'),
+          ),
+          Card(
+            child: Text('Lower One'),
+          )
+        ],
+      ),
+    );
+  }
+}
